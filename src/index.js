bff2b0f202518c51483f734f0a8e84d0454f45c5
AussieSaz13 <aussiesaz13@gmail.com>
2024-04-26 18:18:10 -0400

    updated html

--- src/index.js
+++ src/index.js
@@ -55,3 +55,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+let forecast = document.querySelector("#forecast");
